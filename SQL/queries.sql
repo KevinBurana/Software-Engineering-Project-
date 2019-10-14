eab2c31c4d5c183ae546213e9aadb4972840f360
ShaanBhatt <56207132+ShaanBhatt@users.noreply.github.com>
2019-10-14 13:26:09 -0700

    Update queries.sql

--- SQL/queries.sql
+++ SQL/queries.sql
@@ -1,4 +1,5 @@
 /******
+* Shaan Bhatt
 * Activity Finder
 * Last Edited: 10-5-19  
 ******/
@@ -8,3 +9,19 @@ select trail_name
 from trail t, region r
 where t.reg_id = r.reg_id;
 
+--Query: Finds wildlife in a specific trail.
+select Animal_name
+from WildLife w, Region r, Trail t
+where w.reg_id = r.reg_id
+and   r.trail_id = t.trail_id;
+
+--Query: Finds all the activities in a region.
+
+select Act_name
+from Activity a, Trail t, Region r
+where a.Trail_id = t.Trail_id
+and   t.Reg_id = r.Reg_id;
+
+--Query: Selects all the Climbing activities in a Region
+
+select 
